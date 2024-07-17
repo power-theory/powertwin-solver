f444d3a80a0315807fb9b248fb6e6b80496ed6cc
Luke Macy <lukemacy1@gmail.com>
2024-07-17 04:04:38 -0600

    added .env.local example, updated run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "killing old docker processes"
+echo "Killing old docker processes"
 docker compose rm -fs
 
-echo "building docker containers"
-docker compose up --build -d
+echo "Loading env variables from './.env.local'"
+export ENV_FILE=./.env.local
+
+echo "Building docker containers"
+docker compose --env-file $ENV_FILE build && docker compose --env-file $ENV_FILE up --detach && docker compose --env-file $ENV_FILE logs --follow
